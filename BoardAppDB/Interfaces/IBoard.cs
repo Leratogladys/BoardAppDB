daba9f59eb54f4b0be3e900fa8d590fe14ed7dd9
Mpati Lephole <220024412@stud.cut.ac.za>
2026-09-26 18:00:38 +0200

    Fix Board model and DBInitializer - build succeeds 0 errors

--- BoardAppDB/Interfaces/IBoard.cs
+++ BoardAppDB/Interfaces/IBoard.cs
@@ -1,6 +1,14 @@
-﻿namespace BoardAppDB.Interfaces
+﻿using BoardAppDB.Models;
+namespace BoardAppDB.Interfaces
 {
-    public class IBoard
+    public interface IBoard
     {
+        IEnumerable<Board> GetBoards();
+        Board? GetBoard(string boardCode);    
+        void AddBoard(Board board);
+        void UpdateBoard(Board board);
+        void DeleteBoard(string boardCode);
+        void Save();
+
     }
 }
